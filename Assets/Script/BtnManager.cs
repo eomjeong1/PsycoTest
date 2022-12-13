b457a4c227d3474b343a044858549796c358fefd
DESKTOP-3S2U01O\USER <jseong444@naver.com>
2022-12-13 23:47:26 +0900

    배열 전

---
+++ Assets/Script/BtnManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Btn1()
+    { 
+    
+    }
+    public void Btn2()
+    { 
+    
+    }
+    public void Btn3()
+    {
+
+    }
+    public void Btn4()
+    {
+
+    }
+    public void Btn5()
+    {
+
+    }
+    public void Btn6()
+    {
+
+    }
+
+}
